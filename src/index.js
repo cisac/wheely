d4ff5d87000d55b53e9bcb884381c99fbe56c51d
Cristian Nantu-Isac <cristianni@gmail.com>
2018-02-13 06:17:25 +0200

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import CarouselControls from './carousel-controls';
-import CarouselStateHOC from './carousel';
-
-const Carousel = CarouselStateHOC(CarouselControls);
-
-export { CarouselStateHOC, CarouselControls };
-export default Carousel;
